c0437745ae073427f9c146615ef4707ffa7792a3
amyleuu <seouoyng@gmail.com>
2025-05-14 18:57:14 +0900

    Create 0514_23969.py

---
+++ 0514_23969.py
@@ -0,0 +1,23 @@
+import sys
+input=sys.stdin.readline
+N,K=map(int,input().split())
+A=list(map(int,input().split()))
+count=0
+ans=-1
+def bubble_sort(arr):
+    global count,ans
+    for i in range (len(arr)-1,0,-1):
+        for j in range (i):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                count+=1
+                if count==K:
+                    ans=0
+                    return
+bubble_sort(A)
+if ans==-1:
+    print(ans)
+else:
+    print(*A)
+                    
+    
